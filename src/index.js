c663587634a8bd5905d41a7fa116b6bc54b33aaf
Gregório Neto <igregoriodoneto@gmail.com>
2022-12-06 09:36:52 -0300

    Construindo método Winner

--- src/index.js
+++ src/index.js
@@ -107,6 +107,7 @@ function calculateWinner(squares) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
+    console.log(squares[a]);
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
